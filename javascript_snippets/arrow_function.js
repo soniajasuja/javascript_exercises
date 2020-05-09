e0f69353a1d0d33650d651f0532d07fff4b62481
sonia jasuja <sjasuja2@sapient.com>
2020-05-09 01:11:19 +0100

    added comments for arrow function

--- javascript_snippets/arrow_function.js
+++ javascript_snippets/arrow_function.js
@@ -1,17 +1,21 @@
 /**
  * 
  * ES6 supports arrow function, refer below for the old & new syntax
+ * Arrow Functions Return Value by Default:
  */
 
 function oldCalculateSum (num1, num2) {
-    console.log(num1 + num2);
+    console.log('Sum Old Function: ', num1 + num2);
 }
 
 oldCalculateSum(2,3);
 
 
 var newCalculateSum = (num1, num2) => {
-    console.log(num1 + num2);
+    console.log('Sum Arrow Function: ', num1 + num2);
 }
 
 newCalculateSum(11, 12)
+
+hello = () => "I am Single Line Arrow Function";
+console.log('Hi ', hello());
